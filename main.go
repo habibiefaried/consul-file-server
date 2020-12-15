2b2c118548bc89e82ec61d18bf771477161bd478
Habibie Faried <habibiefaried@gmail.com>
2020-12-15 16:49:03 +0700

    Uploader success

--- main.go
+++ main.go
@@ -1,17 +1,27 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/gorilla/mux"
+	consulapi "github.com/habibiefaried/consul-file-server/consul"
 	"io"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
 )
 
 func UploadFile(w http.ResponseWriter, r *http.Request) {
-	file, handler, err := r.FormFile("file")
+	folder := "consulfs/"
+
+	if os.Getenv("FOLDER") != "" {
+		folder = os.Getenv("FOLDER")
+		if folder[len(folder)-1] != '/' {
+			folder = folder + "/"
+		}
+	}
+
+	file, _, err := r.FormFile("file")
 	if err != nil {
 		panic(err)
 	}
@@ -23,29 +33,14 @@ func UploadFile(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fileName := "/tmp/" + handler.Filename
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(file)
+	err = consulapi.Upload(folder+keyName, buf.Bytes())
 
-	f, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE, 0666)
 	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-
-	maxSizeInKb := 512
-	if os.Getenv("MAX_FILESIZE") != "" {
-		i, err := strconv.Atoi(os.Getenv("MAX_FILESIZE"))
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			maxSizeInKb = i
-		}
-	}
-
-	if handler.Size/1024 <= 512 {
-		_, _ = io.Copy(f, file)
-		_, _ = io.WriteString(w, "File "+fileName+" is uploaded successfully\n")
+		_, _ = io.WriteString(w, fmt.Sprintf("%v", err))
 	} else {
-		_, _ = io.WriteString(w, fmt.Sprintf("File %v has %vKB, max %vKB\n", fileName, handler.Size/1024, maxSizeInKb))
+		_, _ = io.WriteString(w, "File uploaded")
 	}
 }
 
@@ -65,5 +60,6 @@ func main() {
 	if os.Getenv("PORT") != "" {
 		httpPort = os.Getenv("PORT")
 	}
-	log.Fatal(http.ListenAndServe(":"+httpPort, router))
+	log.Println("Listening on port " + httpPort)
+	log.Print(http.ListenAndServe(":"+httpPort, router))
 }
