5a619b84e8df76780d666c5da8d7f784a69c5838
Habibie Faried <habibiefaried@gmail.com>
2020-12-15 15:30:19 +0700

    Uploader

--- Makefile
+++ Makefile
@@ -5,9 +5,12 @@ build:
 	chmod +x main
 
 test:
+	curl -L http://speedtest.ftp.otenet.gr/files/test1Mb.db -o test1Mb.db
+	curl -L http://speedtest.ftp.otenet.gr/files/test100k.db -o test100k.db
+	curl -X POST http://localhost:8081 -F file=@test1Mb.db -F key=test
+	curl -X POST http://localhost:8081 -F file=@test100k.db -F key=test
 	go clean -testcache ./...
 	go test -v ./...
 
 format:
 	go fmt ./...
-	
--- main.go
+++ main.go
@@ -7,32 +7,63 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 func UploadFile(w http.ResponseWriter, r *http.Request) {
 	file, handler, err := r.FormFile("file")
-	fileName := r.FormValue("file_name")
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
 
-	f, err := os.OpenFile(handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+	keyName := r.FormValue("key")
+	if keyName == "" {
+		_, _ = io.WriteString(w, "Variable 'key' must be provided\n")
+		return
+	}
+
+	fileName := "/tmp/" + handler.Filename
+
+	f, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE, 0666)
 	if err != nil {
 		panic(err)
 	}
 	defer f.Close()
-	_, _ = io.WriteString(w, "File "+fileName+" Uploaded successfully")
-	_, _ = io.Copy(f, file)
+
+	maxSizeInKb := 512
+	if os.Getenv("MAX_FILESIZE") != "" {
+		i, err := strconv.Atoi(os.Getenv("MAX_FILESIZE"))
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			maxSizeInKb = i
+		}
+	}
+
+	if handler.Size/1024 <= 512 {
+		_, _ = io.Copy(f, file)
+		_, _ = io.WriteString(w, "File "+fileName+" is uploaded successfully\n")
+	} else {
+		_, _ = io.WriteString(w, fmt.Sprintf("File %v has %vKB, max %vKB\n", fileName, handler.Size/1024, maxSizeInKb))
+	}
 }
 
 func homeLink(w http.ResponseWriter, r *http.Request) {
-	_, _ = fmt.Fprintf(w, "Welcome home!")
+	vars := mux.Vars(r)
+	str := fmt.Sprintf("You are hitting %s path", vars["filename"])
+	_, _ = fmt.Fprintf(w, str)
 }
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", homeLink)
-	router.HandleFunc("/file", UploadFile).Methods("POST")
-	log.Fatal(http.ListenAndServe(":8081", router))
+	router.HandleFunc("/", homeLink).Methods("GET", "HEAD")
+	router.HandleFunc("/{filename}", homeLink).Methods("GET", "HEAD")
+	router.HandleFunc("/", UploadFile).Methods("POST")
+
+	httpPort := "8081"
+	if os.Getenv("PORT") != "" {
+		httpPort = os.Getenv("PORT")
+	}
+	log.Fatal(http.ListenAndServe(":"+httpPort, router))
 }
